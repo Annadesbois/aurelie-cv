89ffb7c362d38fdde06a9e73c93af4fcd54d3558
Aurelie <aurelie.tacquard@gmail.com>
2025-09-25 10:36:22 +0100

    add toggleTheme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,19 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import NavLinks from "./NavLinks";
 import { BsMoonFill, BsSunFill } from "react-icons/bs";
 
+const toggleTheme = () => {
+  const themes = {
+    winter: "winter",
+    forest: "forest",
+  };
+
+  const currentTheme = localStorage.getItem("theme") || themes.winter;
+  const newTheme =
+    currentTheme === themes.forest ? themes.winter : themes.forest;
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+};
+
 const Navbar = () => {
   return (
     <nav className="bg-base-200 px-[1.5rem] md:px-[8rem] sticky top-0 z-50">
@@ -23,6 +36,13 @@ const Navbar = () => {
           <ul className="menu menu-horizontal space-x-4">
             <NavLinks />
           </ul>
+        </div>
+        <div className="navbar-end">
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={toggleTheme} />
+            <BsSunFill className="swap-on w-4 h-4 text-primary" />
+            <BsMoonFill className="swap-off w-4 h-4 text-primary" />
+          </label>
         </div>
       </div>
     </nav>

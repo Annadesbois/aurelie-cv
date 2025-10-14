4e353d25da02bbda6ea9dee61547f9970f34fbb3
Aurelie <aurelie.tacquard@gmail.com>
2025-10-14 16:13:17 +0100

    add handleSubmit

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,25 @@
+import { useForm, ValidationError } from "@formspree/react";
+import { Link } from "react-router-dom";
+
 const ContactPage = () => {
+  const [state, handleSubmit] = useForm("xblzpnlw");
+
+  if (state.succeeded) {
+    return (
+      <div className="bg-base-100 px-[2rem] md:px-[8rem] lg:px-[10rem] py-12">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-4xl font-bold text-primary mb-12">Thanks!</h2>
+          <p className="text-neutral-400 mb-16">
+            Your message has been sent. I’ll get back to you shortly.
+          </p>
+          <Link to="/" className="btn btn-primary rounded-md">
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-base-100 px-[2rem] md:px-[8rem] lg:px-[10rem] py-12 relative">
       <div className="max-w-2xl mx-auto text-center">
@@ -8,7 +29,13 @@ const ContactPage = () => {
         </p>
       </div>
 
-      <form className="mx-auto mt-12 max-w-xl">
+      <form onSubmit={handleSubmit} className="mx-auto mt-12 max-w-xl">
+        <input
+          type="hidden"
+          name="_subject"
+          value="New message from aurelie-cv"
+        />
+
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <div>
             <label
@@ -56,6 +83,11 @@ const ContactPage = () => {
               className="input input-bordered rounded-md w-full bg-base-100"
               required
             />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
 
           <div className="sm:col-span-2">
@@ -71,20 +103,27 @@ const ContactPage = () => {
               rows={4}
               className="textarea textarea-bordered rounded-md w-full bg-base-100"
               required
-            ></textarea>
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
         </div>
 
         <div className="mt-8">
           <button
             type="submit"
+            disabled={state.submitting}
             className="btn btn-primary w-full rounded-md"
           >
-            Send
+            {state.submitting ? "Sending…" : "Send"}
           </button>
         </div>
       </form>
     </div>
   );
-}
-export default ContactPage
+};
+
+export default ContactPage;
